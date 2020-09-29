179d62790a56ed2ce87cd371827a798f38baeac8
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2020-09-29 14:44:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function setup() {
 // NeuralNetwork Creation
     nn = new Dann(4,4);
     nn.addHiddenLayer(16,leakyReLU);
+    nn.addHiddenLayer(8,sigmoid);
     nn.addHiddenLayer(16,leakyReLU);
     nn.makeWeights();
     nn.activation(2,sigmoid);
