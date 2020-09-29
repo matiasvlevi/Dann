9e82d5815bd9e35aee8c267bf3cdc5ea6f2682c7
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2020-09-29 15:18:05 -0400

    Update BinaryCount_4bitsData.js

--- BinaryCount_4bitsData.js
+++ BinaryCount_4bitsData.js
@@ -48,19 +48,20 @@ let data = [
         target: [1,1,0,0]
     },
 
-    {
-        inputs: [1,1,0,1],
-        target: [1,1,1,0]
-    },
+    
 
     {
         inputs: [1,1,1,0],
         target: [1,1,1,1]
-    },
+    }
 
 ];
 
 let testData = [
+    {
+        inputs: [1,1,0,1],
+        target: [1,1,1,0]
+    },
     {
         inputs: [1,1,0,0],
         target: [1,1,0,1]
