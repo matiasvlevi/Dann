db41b91950aff3855c049ea8b47ac62ec23881e0
matiasvlevi <matiasvlevi@gmail.com>
2020-12-27 17:55:11 -0500

    Removing node_modules

--- node_modules/mathjs/lib/esm/entry/dependenciesAny/dependenciesChainClass.generated.js
+++
@@ -1,8 +0,0 @@
-/**
- * THIS FILE IS AUTO-GENERATED
- * DON'T MAKE CHANGES HERE
- */
-import { createChainClass } from '../../factoriesAny.js';
-export var ChainDependencies = {
-  createChainClass
-};
--- node_modules/mathjs/lib/esm/expression/keywords.js
+++
@@ -1,4 +0,0 @@
-// Reserved keywords not allowed to use in the parser
-export var keywords = {
-  end: true
-};
