b1b36238e249680a752db486ad8d1e683437922d
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-01-23 23:18:31 -0500

    public gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: [
+                    'src/imports/init.js',
+                    'src/dom/dom.js',
+                    'src/functions/mathfuncs.js',
+                    'src/functions/activations.js',
+                    'src/functions/lossfuncs.js',
+                    'src/functions/poolfuncs.js',
+                    'src/prototypes/matrix.js',
+                    'src/prototypes/layer.js',
+                    'src/prototypes/dann.js',
+                    'src/exports/exports.js'
+                ],
+                dest: 'build/dann.js'
+            },
+        },
+        terser: {
+
+            src: {
+                files: [{
+                    src: 'build/dann.js',
+                    dest: 'build/dann.min.js'
+                }]
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-terser');
+}
