1add0ea965bcee05e6144b0f785d66e825b90e94
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-01-24 23:30:58 -0500

    init

---
+++ devsetup.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enableDelayedExpansion
+
+:: Create LF containing a line feed character
+set ^"LF=^
+%= This creates a Line Feed character =%
+^"
+
+
+set "multiline=@echo off!LF!echo Nodejs Test Environment!LF!echo.!LF!cd tests!LF!node nodeTest.js!LF!pause"
+echo !multiline!
+echo !multiline!>test.bat
+
+set "multiline=<html>!LF!    <head>!LF!        <script src="build/dann.js" type="text/javascript"></script>!LF!        <script src="tests/browserTest.js" type="text/javascript"></script>!LF!        <title>Browser Test Environment</title>!LF!    </head>!LF!    <body>!LF!    </body>!LF!</html>"
+echo !multiline!
+echo !multiline!>test.htm
+
+
+mkdir tests
+cd tests
+
+set "multiline=const dn = require('../build/dann.min.js');!LF!const Dann = dn.dann;!LF!const Layer = dn.layer;!LF!const Matrix = dn.matrix;!LF!const activations = dn.activations;!LF!const lossfuncs = dn.lossfuncs;!LF!const poolfuncs = dn.poolfuncs;!LF!//__________________________ // NODEJS TEST BELOW // __________________________//"
+echo !multiline!
+echo !multiline!>nodeTest.js
+
+echo //__________________________ // BROWSER TEST BELOW // __________________________// > browserTest.js
+cd..
+cls
+npm install
+pause
+del setup.bat
