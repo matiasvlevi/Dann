6f12401f15e991ea1420edd47b480b0c07a2fc9a
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-01-25 11:37:38 -0500

    math functions converted to es6

--- src/functions/mathfuncs.js
+++ src/functions/mathfuncs.js
@@ -1,38 +1,16 @@
 //Shortening Mathjs functions:
-function random(x1,x2) {
-    return Math.random(1)*(x2-x1)+x1;
-}
-function exp(x1) {
-    return Math.exp(x1);
-}
-function abs(x1) {
-    return Math.abs(x1);
-}
-function log(x1) {
-    return Math.log(x1);
-}
-function pow(x1,e) {
-    return Math.pow(x1,e);
-}
-function sin(x1) {
-    return Math.sin(x1);
-}
-function cos(x1) {
-    return Math.cos(x1);
-}
-function tan(x1) {
-    return Math.tan(x1);
-}
-function round(x1) {
-    return Math.round(x1);
-}
-function sqrt(x1) {
-    return Math.sqrt(x1);
-}
+const random = (a,b) => Math.random(1)*(b-a)+a;
+const exp = (x) => Math.exp(x);
+const abs = (x) => Math.abs(x);
+const log = (x) => Math.log(x);
+const logn = (x,n) => log(x)/log(n);
+const pow = (x,e) => Math.pow(x,e);
+const sin = (x) => Math.sin(x);
+const cos = (x) => Math.cos(x);
+const tan = (x) => Math.tan(x);
+const round = (x) => Math.round(x);
+const sqrt = (x) => Math.sqrt(x);
+
 //Other math functions:
-function cosh(x) {
-    return ((exp(x)+exp(-x))/2);
-}
-function sech(x) {
-    return 1/cosh(x);
-}
+const cosh = (x) => (exp(x)+exp(-x))/2;
+const sech = (x) => 1/cosh(x);
