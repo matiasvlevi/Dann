911a43beca879812c46e9e3b97f09a6a448e7db9
wxxw12 <50473168+matiasvlevi@users.noreply.github.com>
2021-05-24 00:36:48 -0400

    DannError colors

--- src/core/error/DannError.js
+++ src/core/error/DannError.js
@@ -5,24 +5,44 @@ DannError = function DannError(msg, method) {
 
 // Non-static
 DannError.prototype.warn = function () {
-  console.warn('DannError: ' + this.msg);
-  console.warn('> ' + this.method);
+  if (isBrowser) {
+    console.error('DannWarning: ' + this.msg);
+    console.error('> ' + this.method);
+  } else {
+    console.error('\x1b[33m' + 'DannWarning: ' + this.msg + '\x1b[0m');
+    console.error('\x1b[33m' + '> ' + this.method + '\x1b[0m');
+  }
 };
 
 DannError.prototype.error = function () {
-  console.error('DannError: ' + this.msg);
-  console.error('> ' + this.method);
+  if (isBrowser) {
+    console.error('DannError: ' + this.msg);
+    console.error('> ' + this.method);
+  } else {
+    console.error('\x1b[31m' + 'DannError: ' + this.msg + '\x1b[0m');
+    console.error('\x1b[31m' + '> ' + this.method + '\x1b[0m');
+  }
   console.trace();
 };
 
 // Static
 DannError.warn = function (warning, method) {
-  console.warn('DannError: ' + warning);
-  console.warn('> ' + method);
+  if (isBrowser) {
+    console.error('DannWarning: ' + warning);
+    console.error('> ' + method);
+  } else {
+    console.error('\x1b[33m' + 'DannWarning: ' + warning + '\x1b[0m');
+    console.error('\x1b[33m' + '> ' + method + '\x1b[0m');
+  }
 };
 
 DannError.error = function (error, method) {
-  console.error('DannError: ' + error);
-  console.error('> ' + method);
+  if (isBrowser) {
+    console.error('DannError: ' + error);
+    console.error('> ' + method);
+  } else {
+    console.error('\x1b[31m' + 'DannError: ' + error + '\x1b[0m');
+    console.error('\x1b[31m' + '> ' + method + '\x1b[0m');
+  }
   console.trace();
 };
