28973144dbfc14e96a267af2b65ab0d482a7ebd5
wxxw12 <50473168+matiasvlevi@users.noreply.github.com>
2021-05-29 17:59:26 -0400

    Sinc & Softsign tests

--- test/unit/core/functions/actfuncs.js
+++ test/unit/core/functions/actfuncs.js
@@ -4,15 +4,15 @@ suite('', function () {
       test("Testing 'sigmoid' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [0.018, 0.119, 0.5, 0.881, 0.982];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.sigmoid(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.sigmoid(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
       test("Testing 'sigmoid_d' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [0.018, 0.105, 0.25, 0.105, 0.018];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.sigmoid_d(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.sigmoid_d(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
     });
@@ -20,15 +20,15 @@ suite('', function () {
       test("Testing 'tanH' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [-0.999, -0.964, 0, 0.964, 0.999];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.tanH(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.tanH(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
       test("Testing 'tanH_d' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [0.001, 0.071, 1, 0.071, 0.001];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.tanH_d(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.tanH_d(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
     });
@@ -36,15 +36,15 @@ suite('', function () {
       test("Testing 'siLU' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [-0.072, -0.238, 0, 1.762, 3.928];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.siLU(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.siLU(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
       test("Testing 'siLU_d' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [-0.053, -0.091, 0.5, 1.091, 1.053];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.siLU_d(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.siLU_d(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
     });
@@ -52,15 +52,15 @@ suite('', function () {
       test("Testing 'leakyReLU' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [-0.04, -0.02, 0, 2, 4];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.leakyReLU(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.leakyReLU(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
       test("Testing 'leakyReLU_d' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [0.01, 0.01, 1, 1, 1];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.leakyReLU_d(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.leakyReLU_d(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
     });
@@ -68,15 +68,47 @@ suite('', function () {
       test("Testing 'reLU' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [0, 0, 0, 2, 4];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.reLU(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.reLU(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
       test("Testing 'reLU_d' activation function", function () {
         let pointsX = [-4, -2, 0, 2, 4];
         let ans_hardcoded = [0, 0, 1, 1, 1];
-        for (let i = 0; i < pointsX; i++) {
-          assert.equal(act.reLU(pointsX[i]), ans_hardcoded[i]);
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.reLU_d(pointsX[i]), ans_hardcoded[i], 0.01);
+        }
+      });
+    });
+    suite('sinc', function () {
+      test("Testing 'sinc' activation function", function () {
+        let pointsX = [-4, -2, 0, 2, 4];
+        let ans_hardcoded = [-0.189, 0.455, 1, 0.455, -0.189];
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.sinc(pointsX[i]), ans_hardcoded[i], 0.01);
+        }
+      });
+      test("Testing 'sinc_d' activation function", function () {
+        let pointsX = [-4, -2, 0, 2, 4];
+        let ans_hardcoded = [0.116, 0.435, 0, -0.435, -0.116];
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.sinc_d(pointsX[i]), ans_hardcoded[i], 0.01);
+        }
+      });
+    });
+    suite('softsign', function () {
+      test("Testing 'sinc' activation function", function () {
+        let pointsX = [-4, -2, 0, 2, 4];
+        let ans_hardcoded = [-0.8, -0.667, 0, 0.667, 0.8];
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.softsign(pointsX[i]), ans_hardcoded[i], 0.01);
+        }
+      });
+      test("Testing 'softsign_d' activation function", function () {
+        let pointsX = [-4, -2, 0, 2, 4];
+        let ans_hardcoded = [0.04, 0.111, 1, 0.111, 0.04];
+        for (let i = 0; i < pointsX.length; i++) {
+          assert.closeTo(act.softsign_d(pointsX[i]), ans_hardcoded[i], 0.01);
         }
       });
     });
