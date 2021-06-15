6263cf8c6a711b5f61e26b61c0cb88390d5b61ca
wxxw12 <50473168+matiasvlevi@users.noreply.github.com>
2021-06-15 11:25:28 -0400

    Add methods

---
+++ src/core/addFunctions/activation.js
@@ -0,0 +1,42 @@
+/**
+ * Add a custom activation function.
+ * @method activation
+ * @param {string} name the name of the new activation function.
+ * @param {function} activation the activation function.
+ * @param {function} derivative the derivative of this activation function.
+ * @example
+ * <code>
+ * Add.activation('myfunc',
+ *    (x) => {
+ *		if (x <= 0) {
+ *    		return 0;
+ *    	} else {
+ *    		return 1;
+ *    	}
+ *	},
+ *	(x) => {
+ *		return 0;
+ *	}
+ * );
+ * let nn = new Dann();
+ * nn.outputActivation('myfunc');
+ * nn.log();
+ * </code>
+ */
+Add.activation = function (name, activation, derivative) {
+  if (typeof name !== 'string') {
+    DannError.error('The name argument is not a string.', 'Add.activation');
+    return;
+  }
+  if (activation.length !== 1 || derivative.length !== 1) {
+    DannError.error(
+      'One of the functions specified does not have only 1 argument.',
+      'Add.activation'
+    );
+    return;
+  } else {
+    activations[name] = activation;
+    activations[name + '_d'] = derivative;
+    return;
+  }
+};
---
+++ src/core/addFunctions/loss.js
@@ -0,0 +1,41 @@
+/**
+ * Add a custom loss function.
+ * @method loss
+ * @param {string} name the name of the new loss function.
+ * @param {function} loss the loss function.
+ * @example
+ * <code>
+ * Add.loss('myfunc',
+ * (predictions, target) => {
+ *   let sum = 0;
+ *   let ans = 0;
+ *   let n = target.length;
+ *   for (let i = 0; i < n; i++) {
+ *     let y = target[i];
+ *     let yHat = predictions[i];
+ *     sum += abs(y - yHat);
+ *   }
+ *   ans = sum / n;
+ *   return ans;
+ * }
+ * );
+ * let nn = new Dann();
+ * nn.setLossFunction('myfunc');
+ * nn.log();
+ * </code>
+ */
+Add.loss = function (name, loss) {
+  if (typeof name !== 'string') {
+    DannError.error('The name argument is not a string.', 'Add.loss');
+    return;
+  }
+  if (loss.length === 2) {
+    lossfuncs[name] = loss;
+  } else {
+    DannError.error(
+      'The loss function specified can only have 2 argument.',
+      'newActivation'
+    );
+    return;
+  }
+};
