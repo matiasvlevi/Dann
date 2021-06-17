e90eff3b0a4fcb0a0ed9f24e04d7bf6244e991a7
wxxw12 <50473168+matiasvlevi@users.noreply.github.com>
2021-06-17 01:34:45 -0400

    more Add class unit tests

--- test/unit/core/functions/add.js
+++ test/unit/core/functions/add.js
@@ -1,9 +1,13 @@
 suite('', function () {
   suite('Add custom functions', function () {
     suite('Activations', function () {
+      let func;
+      let func_d;
+      setup(function () {
+        func = (x) => x * x;
+        func_d = (x) => 2 * x;
+      });
       test('Sould have added an activation', function () {
-        let func = (x) => x * x;
-        let func_d = (x) => 2 * x;
         Add.activation('mynewfunc', func, func_d);
         assert.exists(act.mynewfunc);
         assert.exists(act.mynewfunc_d);
@@ -11,27 +15,44 @@ suite('', function () {
         assert.equal(act.mynewfunc_d(3), func_d(3));
       });
       test('Should not work with more than one argument for activation', function () {
-        let func = (x, y) => x * x;
-        let func_d = (x) => 2 * x;
-        Add.activation('mynewfunc2', func, func_d);
+        let func_twoargs = (x, y) => x * x;
+        let func_twoargs_d = (x) => 2 * x;
+        Add.activation('mynewfunc2', func_twoargs, func_twoargs_d);
         assert.notExists(act.mynewfunc2);
         assert.notExists(act.mynewfunc2_d);
       });
+      test('Should apply to a Dann model', function () {
+        const nn = new Dann();
+        nn.outputActivation('mynewfunc');
+        assert.equal(nn.Layers[1].actname, 'mynewfunc');
+        assert.equal(nn.Layers[1].actfunc.toString(), func.toString());
+        assert.equal(nn.Layers[1].actname_d, 'mynewfunc_d');
+        assert.equal(nn.Layers[1].actfunc_d.toString(), func_d.toString());
+      });
     });
     suite('Loss', function () {
+      let func;
+      setup(function () {
+        func = (x, y) => x[0] - y[0];
+      });
       test('Sould have added a loss function', function () {
-        let func = (x, y) => x[0] - y[0];
         Add.loss('mynewfunc', func);
         assert.exists(lossfuncs.mynewfunc);
         assert.equal(lossfuncs.mynewfunc([4], [2]), func([4], [2]));
       });
       test('Should not work with less or more than two argument for loss function', function () {
-        let func = (x, y, z) => x[0] - y[0];
-        Add.loss('mynewfunc2', func);
+        let func_threeargs = (x, y, z) => x[0] - y[0];
+        Add.loss('mynewfunc2', func_threeargs);
         assert.notExists(lossfuncs.mynewfunc2);
-        let func2 = (x) => x[0];
-        Add.loss('mynewfunc3', func2);
+        let func_onearg = (x) => x[0];
+        Add.loss('mynewfunc3', func_onearg);
         assert.notExists(lossfuncs.mynewfunc3);
+      });
+      test('Should apply to a Dann model', function () {
+        const nn = new Dann();
+        nn.setLossFunction('mynewfunc');
+        assert.equal(nn.lossfunc.toString(), func.toString());
+        assert.equal(nn.lossfunc_s, 'mynewfunc');
       });
     });
   });
