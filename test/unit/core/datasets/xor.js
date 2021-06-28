192557c136cb7d9ee84461e73e38481b3ca9376b
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-06-28 13:16:13 -0400

    format unit test xor

--- test/unit/core/datasets/xor.js
+++ test/unit/core/datasets/xor.js
@@ -1,46 +1,32 @@
-suite('My series of tests', function () {
-  const makeXOR = Dannjs.makeXOR;
-  xor2 = [
-    { input: [0, 0], output: [0] },
-    { input: [0, 1], output: [1] },
-    { input: [1, 0], output: [1] },
-    { input: [1, 1], output: [0] },
-  ];
-
-  xor3 = [
-    { input: [0, 0, 0], output: [0] },
-    { input: [0, 0, 1], output: [1] },
-    { input: [0, 1, 0], output: [1] },
-    { input: [0, 1, 1], output: [0] },
-    { input: [1, 0, 0], output: [1] },
-    { input: [1, 0, 1], output: [0] },
-    { input: [1, 1, 0], output: [0] },
-    { input: [1, 1, 1], output: [1] },
-  ];
-
-  setup(function () {
-    // setup code
-  });
-  test(`[
-    { input: [0, 0], output: [0] },
-    { input: [0, 1], output: [1] },
-    { input: [1, 0], output: [1] },
-    { input: [1, 1], output: [0] },
-  ]
-  Should equal to makeXOR(2) `, function () {
-    assert.deepEqual(xor2, makeXOR(2));
-  });
-  test(` [
-    { input: [0, 0, 0], output: [0] },
-    { input: [0, 0, 1], output: [1] },
-    { input: [0, 1, 0], output: [1] },
-    { input: [0, 1, 1], output: [0] },
-    { input: [1, 0, 0], output: [1] },
-    { input: [1, 0, 1], output: [0] },
-    { input: [1, 1, 0], output: [0] },
-    { input: [1, 1, 1], output: [1] },
-  ]
-   Should equal to makeXOR(3) `, function () {
-    assert.deepEqual(xor3, makeXOR(3));
+suite('', function () {
+  suite('', function () {
+    suite('makeXOR method', function () {
+      let xor2;
+      let xor3;
+      setup(function () {
+        xor2 = [
+          { input: [0, 0], output: [0] },
+          { input: [0, 1], output: [1] },
+          { input: [1, 0], output: [1] },
+          { input: [1, 1], output: [0] },
+        ];
+        xor3 = [
+          { input: [0, 0, 0], output: [0] },
+          { input: [0, 0, 1], output: [1] },
+          { input: [0, 1, 0], output: [1] },
+          { input: [0, 1, 1], output: [0] },
+          { input: [1, 0, 0], output: [1] },
+          { input: [1, 0, 1], output: [0] },
+          { input: [1, 1, 0], output: [0] },
+          { input: [1, 1, 1], output: [1] },
+        ];
+      });
+      test(`Should equal to makeXOR(2) `, function () {
+        assert.deepEqual(xor2, makeXOR(2));
+      });
+      test(`Should equal to makeXOR(3) `, function () {
+        assert.deepEqual(xor3, makeXOR(3));
+      });
+    });
   });
 });
