df9066fa8b60a7314cc075c1f603e11383628223
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-06-30 23:58:17 -0400

    fixed feed

--- src/classes/rann/methods/feed.js
+++ src/classes/rann/methods/feed.js
@@ -1,48 +1,51 @@
 Rann.prototype.feed = function feed(input, options) {
-  let log = false;
-  if (options !== undefined) {
-    if (options.log !== undefined) {
-      log = options.log;
+  if (Rann.checkSequences(input, this.i)) {
+    let log = false;
+    if (options !== undefined) {
+      if (options.log !== undefined) {
+        log = options.log;
+      }
     }
-  }
-  for (let d = 0; d < input.length; d++) {
-    this.previous = new Matrix(this.h, 1);
-    let input_s = input[d];
-    for (let t = 0; t < input[d].length; t++) {
-      // New input
-      new_input = new Matrix(this.i, 1);
-      new_input.matrix[t][0] = input_s[t];
+    for (let d = 0; d < input.length; d++) {
+      // First previous values
+      this.previous = new Matrix(this.h, 1);
+      // Input sequence
+      let input_s = input[d];
+      // Sequence length
+      let sequence = input[d].length;
+      for (let t = 0; t < sequence; t++) {
+        // New input for sequence
+        new_input = new Matrix(sequence, 1);
+        new_input.matrix[t][0] = input_s[t];
 
-      // Mult input to hidden
-      this.mulu = Matrix.mult(this.U, new_input);
+        // Mult input to hidden
+        this.mulu = Matrix.mult(this.U, new_input);
 
-      // Add bias
-      this.mulu.add(this.bias);
+        // Mult previous hidden to current hidden
+        this.mulw = Matrix.mult(this.W, this.previous);
 
-      // Mult previous hidden to current hidden
-      this.mulw = Matrix.mult(this.W, this.previous);
+        // Add two matrices as a grid
+        let sum = Matrix.add(this.mulw, this.mulu);
 
-      // Add two matrices as a grid.
-      let sum = Matrix.addGrid(Matrix.transpose(this.mulw), this.mulu);
+        // Map to activation function
+        let mapped = Matrix.map(sum, this.actfunc);
 
-      // Matrix.map not working in this case for some reason.
-      let mapped = new Matrix(sum.rows, sum.cols);
-      for (let i = 0; i < sum.rows; i++) {
-        for (let j = 0; j < sum.cols; j++) {
-          let v = sum.matrix[i][j];
-          mapped.matrix[i][j] = this.actfunc(v);
+        this.mulv = Matrix.mult(this.V, mapped);
+        if (log === true) {
+          console.log('Time: ' + t);
+          console.log(this.mulv);
         }
+        this.previous = mapped;
+        this.output = this.mulv;
       }
-      this.mapped = mapped;
-
-      this.mulv = Matrix.mult(mapped, Matrix.transpose(this.V));
-      if (log === true) {
-        console.log('Time: ' + t);
-        console.log(this.mulv);
-      }
-      this.previous = mapped;
-      this.output = this.mulv;
     }
+    let out = Matrix.map(this.output, this.o_actfunc);
+    return Matrix.toArray(out);
+  } else {
+    DannError.error(
+      'Input sequences length must equal the number of input neurons the Rann model has',
+      'Rann.prototype.feed'
+    );
+    return undefined;
   }
-  return this.output;
 };
