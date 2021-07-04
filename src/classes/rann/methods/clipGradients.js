a6fe810d8629fd6a9c14d5f89c165cc090bf38ef
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-04 14:49:29 -0400

    Rann.prototype.clipGradients to avoid gradients explosion

---
+++ src/classes/rann/methods/clipGradients.js
@@ -0,0 +1,36 @@
+Rann.prototype.clipGradients = function clipGradients(min_clip, max_clip) {
+  // Clip maximum
+  let dUmax = this.dU.max();
+  if (dUmax > max_clip) {
+    let s = max_clip / dUmax;
+    this.dU.map((x) => x * s);
+  }
+  let dVmax = this.dV.max();
+  if (dVmax > max_clip) {
+    let s = max_clip / dVmax;
+    this.dV.map((x) => x * s);
+  }
+  let dWmax = this.dW.max();
+  if (dWmax > max_clip) {
+    let s = max_clip / dWmax;
+    this.dW.map((x) => x * s);
+  }
+  // Clip minimum
+  let dUmin = this.dU.min();
+  if (dUmin < min_clip) {
+    let s = min_clip / dUmin;
+    this.dU.map((x) => x * s);
+  }
+  let dVmin = this.dV.min();
+  if (dVmin < min_clip) {
+    let s = min_clip / dVmin;
+    this.dV.map((x) => x * s);
+  }
+  let dWmin = this.dW.min();
+  if (dWmin < min_clip) {
+    let s = min_clip / dWmin;
+    this.dW.map((x) => x * s);
+  }
+
+  return;
+};
