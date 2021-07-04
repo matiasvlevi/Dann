77c7553da04adb83f9372815c1ad13d091b3e152
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-04 14:47:48 -0400

    Matrix.addColumn method

--- src/classes/matrix/methods/addGrid.js
+++ src/classes/matrix/methods/addGrid.js
@@ -10,3 +10,22 @@ Matrix.addGrid = function addGrid(m1, m2) {
   }
   return new Matrix().set(ans);
 };
+
+Matrix.addColumn = function addColumn(m1, m2) {
+  let a;
+  let b;
+  if (m1.cols === 1) {
+    a = m1;
+    b = m2;
+  } else if (m2.cols === 1) {
+    a = m2;
+    b = m1;
+  }
+  let ans = new Matrix(b.rows, b.cols);
+  for (let i = 0; i < b.rows; i++) {
+    for (let j = 0; j < b.cols; j++) {
+      ans.matrix[i][j] = b.matrix[i][j] + a.matrix[i][0];
+    }
+  }
+  return ans;
+};
