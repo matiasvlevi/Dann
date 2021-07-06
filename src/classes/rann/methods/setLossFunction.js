9cc3554f606633b416012a5f5c5f1a99dbf23942
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-06 15:54:31 -0400

    Added setLossFunciton

---
+++ src/classes/rann/methods/setLossFunction.js
@@ -0,0 +1,77 @@
+/**
+ * Set the loss function of a Rann model
+ * @method setLossFunction
+ * @param {String} name Takes a string of the loss function's name. If this function is not called, the loss function will be set to 'mse' by default. See available loss functions <a target="_blank" href="dannjs.org">Here</a>.
+ * <table>
+ * <thead>
+ *   <tr>
+ *     <th>Name</th>
+ *     <th>Desmos</th>
+ *   </tr>
+ * </thead>
+ * <tbody>
+ *   <tr>
+ *     <td>mse</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/msg3bebyhe">See graph</a></td>
+ *   </tr>
+ *   <tr>
+ *     <td>mae</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/sqyudacjzb">See graph</a></td>
+ *   </tr>
+ *   <tr>
+ *     <td>lcl</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/ropuc3y6sa">See graph</a></td>
+ *   </tr>
+ *   <tr>
+ *     <td>mbe</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/xzp1hr0vin">See graph</a></td>
+ *   </tr>
+ *   <tr>
+ *     <td>mael</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/dimqieesut">See graph</a></td>
+ *   </tr>
+ *   <tr>
+ *     <td>rmse</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/x7efwdfada">See graph</a></td>
+ *   </tr>
+ *   <tr>
+ *     <td>mce</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/bzlqe7bafx">See graph</a></td>
+ *   </tr>
+ *   <tr>
+ *     <td>bce</td>
+ *     <td><a target="_blank" href="https://www.desmos.com/calculator/ri1bj9gw4l">See graph</a></td>
+ *   </tr>
+ * </tbody>
+ * </table>
+ * <br/>
+ * See how to add more <a class="hyperlink" href="./Add.html#method_loss">Here</a>
+ * @example
+ * <code>
+ * const rnn = new Rann(4, 21, 4);
+ * rnn.setLossFunction('mael');
+ * rnn.log();
+ * </code>
+ */
+Rann.prototype.setLossFunction = function setLossFunction(name) {
+  let func = lossfuncs[name];
+  if (func === undefined) {
+    if (typeof name === 'string') {
+      DannError.error(
+        "'" +
+          name +
+          "' is not a valid loss function, as a result, the model's loss function is set to 'mse' by default.",
+        'Rann.prototype.setLossFunction'
+      );
+      return;
+    } else {
+      DannError.error(
+        "Did not detect string value, as a result, the loss function is set to 'mse' by default.",
+        'Rann.prototype.setLossFunction'
+      );
+      return;
+    }
+  }
+  this.lossfunc_s = name;
+  this.lossfunc = func;
+};
