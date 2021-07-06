9f4ac47a59ef0040d28f360ec9f6f1afc3ab2c7d
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-06 15:53:40 -0400

    removed useless imports

--- test/manual-tests/node/empty-example/test.js
+++ test/manual-tests/node/empty-example/test.js
@@ -1,9 +1,2 @@
 const dn = require('../../../../build/dann.js');
-const Dann = dn.dann;
-const Layer = dn.layer;
-const Matrix = dn.matrix;
-const activations = dn.activations;
-const lossfuncs = dn.lossfuncs;
-const poolfuncs = dn.poolfuncs;
-const Add = dn.add;
 //__________________________ // NODEJS TEST BELOW // __________________________//
