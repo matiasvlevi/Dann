2594194c36fffabdb2820b68b375df64d9aab209
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2021-07-18 14:11:01 -0400

    added different builds configs

--- src/concatConfig.js
+++ src/concatConfig.js
@@ -1,21 +1,57 @@
 // Source file concat configuration
-module.exports = [
-  // Import dependencies
-  'src/io/head.js',
-  // Core
-  'src/core/**/*.js',
-  // Matrix
-  'src/classes/matrix/constructor.js',
-  'src/classes/matrix/methods/*.js',
-  // Layer
-  'src/classes/layer/constructor.js',
-  'src/classes/layer/methods/*.js',
-  // Dann
-  'src/classes/dann/constructor.js',
-  'src/classes/dann/methods/*.js',
-  // Rann
-  'src/classes/rann/constructor.js',
-  'src/classes/rann/methods/*.js',
-  // Export
-  'src/io/exports.js',
-];
+module.exports = {
+  build: [
+    // Import dependencies
+    'src/io/head.js',
+    // Core
+    'src/core/**/*.js',
+    // Matrix
+    'src/classes/matrix/constructor.js',
+    'src/classes/matrix/methods/*.js',
+    // Layer
+    'src/classes/layer/constructor.js',
+    'src/classes/layer/methods/*.js',
+    // Dann
+    'src/classes/dann/constructor.js',
+    'src/classes/dann/methods/*.js',
+    // Rann
+    'src/classes/rann/constructor.js',
+    'src/classes/rann/methods/*.js',
+    // Export
+    'src/io/exports.js',
+  ],
+  onlyDann: [
+    // Import dependencies
+    'src/io/head.js',
+    // Core
+    'src/core/**/*.js',
+    // Matrix
+    'src/classes/matrix/constructor.js',
+    'src/classes/matrix/methods/*.js',
+    // Layer
+    'src/classes/layer/constructor.js',
+    'src/classes/layer/methods/*.js',
+    // Dann
+    'src/classes/dann/constructor.js',
+    'src/classes/dann/methods/*.js',
+    // Export
+    'src/io/exports.js',
+  ],
+  onlyRann: [
+    // Import dependencies
+    'src/io/head.js',
+    // Core
+    'src/core/**/*.js',
+    // Matrix
+    'src/classes/matrix/constructor.js',
+    'src/classes/matrix/methods/*.js',
+    // Layer
+    'src/classes/layer/constructor.js',
+    'src/classes/layer/methods/*.js',
+    // Rann
+    'src/classes/rann/constructor.js',
+    'src/classes/rann/methods/*.js',
+    // Export
+    'src/io/exports.js',
+  ],
+};
