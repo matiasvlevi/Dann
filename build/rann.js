0c4002727f04ddf5c92029e9cbc6910b69d6f904
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-07-26 23:20:32 -0400

    commit

--- build/rann.js
+++ build/rann.js
@@ -2849,13 +2849,11 @@ Rann.prototype.unNormalizeSequence = function unNormalizeSequence(sequence) {
  * @param {Number} sequence The sequence length
  * @return {Boolean} If the sequences are valid for the given Rann model
  */
-Rann.prototype.validateSequences = function checkSequences(input) {
+Rann.prototype.validateSequences = function validateSequences(input) {
   for (let i = 0; i < input.length; i++) {
-    let type = typeof input[i];
-    if (type === 'number') {
-      if (input[i].length !== this.i) {
-        return false;
-      }
+    //let type = typeof input[i];
+    if (input[i].length !== this.i) {
+      return false;
     }
   }
   return true;
--- src/classes/rann/methods/validateSequences.js
+++ src/classes/rann/methods/validateSequences.js
@@ -5,13 +5,11 @@
  * @param {Number} sequence The sequence length
  * @return {Boolean} If the sequences are valid for the given Rann model
  */
-Rann.prototype.validateSequences = function checkSequences(input) {
+Rann.prototype.validateSequences = function validateSequences(input) {
   for (let i = 0; i < input.length; i++) {
-    let type = typeof input[i];
-    if (type === 'number') {
-      if (input[i].length !== this.i) {
-        return false;
-      }
+    //let type = typeof input[i];
+    if (input[i].length !== this.i) {
+      return false;
     }
   }
   return true;
