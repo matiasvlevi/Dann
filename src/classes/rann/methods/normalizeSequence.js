9d2b027626db622482fe321a5575b967437da4fc
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-07-27 11:49:41 -0400

    fixed normalize sequence method

--- src/classes/rann/methods/normalizeSequence.js
+++ src/classes/rann/methods/normalizeSequence.js
@@ -1,23 +1,24 @@
-Rann.prototype.normalizeSequence = function normalizeSequence(
-  sequence,
-  record
-) {
+Rann.prototype.normalizeSequence = function normalizeSequence(input, record) {
   // Find largest value
   if (record !== undefined) {
     if (record === true) {
       let max_arr = [];
-      for (let i = 0; i < sequence.length; i++) {
-        max_arr.push(Math.max(sequence[i]));
+      for (let i = 0; i < input.length; i++) {
+        max_arr = max_arr.concat(input[i]);
       }
-      this.largestSequenceValue = Math.max(max);
+      let largest = Math.max.apply(1, max_arr);
+      if (this.largestSequenceValue < largest) {
+        this.largestSequenceValue = largest;
+      }
     }
   }
   // Normalize sequence
   let new_sequence = [];
-  for (let i = 0; i < sequence.length; i++) {
+  let sequence_length = input[0].length;
+  for (let i = 0; i < input.length; i++) {
     new_sequence[i] = [];
-    for (let j = 0; j < sequence[0].length; j++) {
-      new_sequence[i].push(sequence[i][j] / this.largestSequenceValue);
+    for (let j = 0; j < sequence_length; j++) {
+      new_sequence[i].push(input[i][j] / this.largestSequenceValue);
     }
   }
   return new_sequence;
