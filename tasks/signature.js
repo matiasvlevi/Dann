c4dc0a9fd5f5b480d69c20e5cc47595bb685d73c
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-08-13 07:53:32 -0400

    removed sig for npx sig

--- tasks/signature.js
+++
@@ -1,6 +0,0 @@
-const signature = require('./assets/sig');
-module.exports = function(grunt) {
-  grunt.registerMultiTask('sig', ' display signature', () => {
-    grunt.log.writeln(signature)
-  })
-}
