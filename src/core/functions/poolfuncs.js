0b3305849a47576c77ed32bd177ea0093ff002e9
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-08-13 07:18:03 -0400

    pool function names wont collide with p5js function names

--- src/core/functions/poolfuncs.js
+++ src/core/functions/poolfuncs.js
@@ -1,34 +1,31 @@
 // Pooling functions:
-function max(arr) {
-  let record = 0;
-  let len = arr.length;
-  for (let i = 0; i < len; i++) {
-    if (arr[i] > record) {
-      record = arr[i];
-    }
-  }
-  return record;
-}
-function min(arr) {
-  let record = Infinity;
-  let len = arr.length;
-  for (let i = 0; i < len; i++) {
-    if (arr[i] < record) {
-      record = arr[i];
-    }
-  }
-  return record;
-}
-function avg(arr) {
-  let sum = 0;
-  let len = arr.length;
-  for (let i = 0; i < len; i++) {
-    sum += arr[i];
-  }
-  return sum / len;
-}
 let poolfuncs = {
-  max: max,
-  min: min,
-  avg: avg,
+  max: function (arr) {
+    let record = 0;
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      if (arr[i] > record) {
+        record = arr[i];
+      }
+    }
+    return record;
+  },
+  min: function (arr) {
+    let record = Infinity;
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      if (arr[i] < record) {
+        record = arr[i];
+      }
+    }
+    return record;
+  },
+  avg: function (arr) {
+    let sum = 0;
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+      sum += arr[i];
+    }
+    return sum / len;
+  },
 };
