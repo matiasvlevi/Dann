3fd190a2e326c261979d1cde2273626ad0f384fc
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-08-29 13:24:53 -0400

    added examples

--- src/classes/matrix/methods/fillCol.js
+++ src/classes/matrix/methods/fillCol.js
@@ -2,7 +2,14 @@
  * Fill a culmn of a matrix with a value.
  * @method fillCol
  * @param {Number} col The column to fill
- * @param {Number} num The value to fill the column with;
+ * @param {Number} num The value to fill the column with
+ * @chainable
+ * @example
+ * <code>
+ * let m = new Matrix(3, 4);
+ * // Fill column 3 with values of 100
+ * m.fillCol(3, 100);
+ * </code>
  */
 Matrix.prototype.fillCol = function fillCol(col, num) {
   if (col >= this.cols) {
--- src/classes/matrix/methods/fillRow.js
+++ src/classes/matrix/methods/fillRow.js
@@ -5,6 +5,12 @@
  * @param {Number} row The row index to fill
  * @param {Number} num The value to fill the row with
  * @chainable
+ * @example
+ * <code>
+ * let m = new Matrix(3, 4);
+ * // Fill row 2 with values of 100
+ * m.fillRow(2, 100);
+ * </code>
  */
 Matrix.prototype.fillRow = function fillRow(row, num) {
   if (row >= this.rows) {
