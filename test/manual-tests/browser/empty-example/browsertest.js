9a80754c7527887394a2764efa08df6ba6bd6b5a
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-08-29 12:15:25 -0400

    temporary test

--- test/manual-tests/browser/empty-example/browsertest.js
+++ test/manual-tests/browser/empty-example/browsertest.js
@@ -1,1 +1,8 @@
 //__________________________ // BROWSER TEST BELOW // __________________________//
+
+const nn = new Dann(2, 1);
+nn.addHiddenLayer(4);
+nn.addHiddenLayer(3);
+nn.makeWeights();
+// nn.addDropout(0.1);
+nn.log({ weights: true });
