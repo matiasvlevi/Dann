79979a8ca21256c71729b3e571c4b0bbf0b3a428
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-08-29 13:25:04 -0400

    removed temporary test

--- test/manual-tests/browser/empty-example/browsertest.js
+++ test/manual-tests/browser/empty-example/browsertest.js
@@ -1,8 +1,1 @@
 //__________________________ // BROWSER TEST BELOW // __________________________//
-
-const nn = new Dann(2, 1);
-nn.addHiddenLayer(4);
-nn.addHiddenLayer(3);
-nn.makeWeights();
-// nn.addDropout(0.1);
-nn.log({ weights: true });
