22f1b60d5456f13c9a92a7795f4568c22abe1dea
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2021-11-08 19:45:47 -0500

    Validation & Error handling

---
+++ src/classes/dann/methods/Core/validation.js
@@ -0,0 +1,43 @@
+Dann.prototype.checkArrayLength = function checkArrayLength(arr, n) {
+  if (arr.length === n) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Dann.prototype.checkLearningRate = function checkLearningRate() {
+  if (typeof this.lr !== 'number') {
+    DannError.error(
+      'The learning rate specified (Dann.lr property) is not a number.',
+      'Dann.prototype.backpropagate'
+    );
+    return false;
+  } else {
+    if (this.lr >= 1) {
+      DannError.error(
+        'The learning rate specified is greater or equal to 1',
+        'Dann.prototype.backpropagate'
+      );
+      return false;
+    }
+  }
+  return true;
+};
+
+Dann.prototype.checkDropoutRate = function (dropout = undefined) {
+  if (dropout >= 1) {
+    DannError.error(
+      'The probability value can not be bigger or equal to 1',
+      'Dann.prototype.backpropagate'
+    );
+    return false;
+  } else if (dropout <= 0) {
+    DannError.error(
+      'The probability value can not be smaller or equal to 0',
+      'Dann.prototype.backpropagate'
+    );
+    return false;
+  }
+  return true;
+};
